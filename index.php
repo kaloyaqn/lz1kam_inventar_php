068cdbaa8c652b1fe2b833e13c2d6d25c6d13297
Kaloyan Angelov <64629762+kaloyaqn@users.noreply.github.com>
2023-02-20 08:15:36 +0200

    feat: fixed login page

--- index.php
+++ index.php
@@ -1,6 +1,30 @@
-<?php include "components/header.php" ?>
+<?php 
+include "components/header.php" ;
+require_once ("config.php"); 
 
-<?php require_once ("config.php"); ?>
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = mysqli_real_escape_string($conn, $_POST["username"]);
+    $password = mysqli_real_escape_string($conn, $_POST["password"]);
+
+    $sql = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) == 1) {
+        header("Location: pages/dashboard.php");
+        exit();
+    } else {
+        echo "
+        <div class='alert alert-danger' role='alert'>
+        Грешно потребителско име или парола! Моля върнете се обратно или опитайте по-късно.
+        </div>
+
+        <button class='btn btn-primary w-100'><a class='text-white text-decoration-none' href='index.php'>Върни се обратно</a></button>
+        ";        }
+}
+
+mysqli_close($conn);
+?>
 
     <div class="container-sm vh-100 d-flex justify-content-center align-items-center">
         <form action="login.php" method="post">

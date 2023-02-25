9953d051c974f9603b03d63bf5fb6ac568de25e6
kalo69 <kallnoob11@gmail.com>
2023-02-25 13:39:06 +0200

    fix: updated config

--- config.php
+++ config.php
@@ -1,11 +1,12 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "lz_inv";
+$username = "ttonevco_lz1kamuser";
+$password = "lodkazabokluk11!"; 
+$dbname = "ttonevco_lz1kamdbsklad";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
+mysqli_set_charset($conn,"utf8");
 
 if ($conn->connect_error) {
     die("Грешка при свързване с датабазата: " . $conn->connect_error);
